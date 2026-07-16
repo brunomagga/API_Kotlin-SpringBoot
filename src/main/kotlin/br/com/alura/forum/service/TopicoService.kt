5f5a30dff7a89b1847d0dec23ab35a84a4cd0d9f
brunomagga <brunomagga2007@gmail.com>
2026-07-16 17:29:55 -0300

    Implementando controllers

---
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -0,0 +1,25 @@
+package br.com.alura.forum.service
+
+import br.com.alura.forum.model.Curso
+import br.com.alura.forum.model.Topico
+import br.com.alura.forum.model.Usuario
+import org.springframework.stereotype.Service
+import java.util.Arrays
+
+@Service
+class TopicoService(private var topicos: List<Topico> = ArrayList()) {
+
+    fun listar(): List<Topico> {
+        return topicos
+    }
+
+    fun buscarPorId(id: Long): Topico {
+        return topicos.stream().filter ({
+            t->t.id == id
+        }).findFirst().get()
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos.plus(topico)
+    }
+}
